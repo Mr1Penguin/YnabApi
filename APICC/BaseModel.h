a662999c57d567c0405a45ad5161e371c8fc36fd
Igor Sharapov <igorsharapov@outlook.com>
2018-08-09 15:04:03 +0300

    Add base code for serialization/deserialization

--- APICC/BaseModel.h
+++ APICC/BaseModel.h
@@ -1,11 +1,51 @@
 #pragma once
 
-#include <string.h>
+#include <rapidjson\document.h>
+#include <rapidjson\stringbuffer.h>
+#include <rapidjson\writer.h>
+#include <type_traits>
+
+using rwriter = rapidjson::Writer<rapidjson::GenericStringBuffer<rapidjson::UTF16<>>, rapidjson::UTF16<>>;
+using rvalue = rapidjson::GenericValue<rapidjson::UTF16<>>;
 
 namespace apicc {
 	struct BaseModel {
-		
 	protected:
+		template<class T>
+		void write(winrt::Windows::Foundation::IReference<T> obj, rwriter & writer, wchar_t * key,
+			rapidjson::SizeType keyLength, bool forceNull = false) {
+			if (key != nullptr && (obj || forceNull))
+				writer.Key(key, keyLength);
 
+			if (!obj) {
+				if (forceNull)
+					writer.Null();
+				return;
+			}
+
+			if constexpr (std::is_same_v<int32_t, T>) {
+				writer.Int(obj.GetInt32());
+			}
+		}
+
+		template<class T>
+		winrt::Windows::Foundation::IReference<T> read(const rvalue & document, wchar_t * key) {
+			winrt::Windows::Foundation::IReference<T> obj = nullptr;
+			const rvalue * val = &document;
+			if (key != nullptr) {
+				auto it = val->FindMember(key);
+				if (it == val->MemberEnd() || it->value.IsNull()) {
+					return obj;
+				}
+
+				val = &it->value;
+			}
+
+			if constexpr(std::is_same_v<T, int32_t>) {
+				obj = val->GetInt();
+			}
+
+			return obj;
+		}
 	};
 }
